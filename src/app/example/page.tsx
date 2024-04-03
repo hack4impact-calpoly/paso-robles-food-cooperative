fab24de54478d08207e35e3f11204d89ffff8751
upluke <lukeforadream@gmail.com>
2024-04-02 23:31:35 -0700

    added tailwind package

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,4 +1,10 @@
 // page template
 export default function Example() {
-  return <div>This is an example page using App Router!</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        This is an example page using App Router!
+      </h1>
+    </div>
+  );
 }
